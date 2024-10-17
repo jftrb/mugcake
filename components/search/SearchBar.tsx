d37a5010a5fd377a0a271685f314d86586d59a1e
DESKTOP-UOE0EAV\M <jeanfelix1@hotmail.fr>
2024-10-17 12:13:14 -0400

    Added clear button to search box

--- components/search/SearchBar.tsx
+++ components/search/SearchBar.tsx
@@ -1,28 +1,35 @@
-import { StyleSheet, TextInput } from "react-native";
+import { Pressable, StyleSheet, Text, TextInput } from "react-native";
 import { ImageButton } from "../ImageButton";
 import { ThemedText } from "../ThemedText";
 import { ThemedView } from "../ThemedView";
 import { useState } from "react";
 import { router } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons";
 
-export default function SearchBar({query} : {query: string}) {
-  const [searchText, setSearchText] = useState(query)
-  
+export default function SearchBar({ query }: { query: string }) {
+  const [searchText, setSearchText] = useState(query);
+
   function submitSearch(query: string) {
-    console.log(`Search for : ${query}`)
+    console.log(`Search for : ${query}`);
     const searchParams = query === "" ? query : `?query=${query}`;
-    router.navigate(`./search${searchParams}`)
+    router.navigate(`./search${searchParams}`);
   }
 
   return (
     <ThemedView style={styles.titleContainer}>
       <ThemedText type="title">Search</ThemedText>
-      <TextInput
-        style={styles.searchBox}
-        defaultValue={query}
-        onChangeText={setSearchText}
-        onSubmitEditing={() => submitSearch(searchText)}
-      />
+      <ThemedView style={styles.searchContainer}>
+        <TextInput
+        style={[styles.searchBox, {}]}
+          defaultValue={query}
+          onChangeText={setSearchText}
+          onSubmitEditing={() => submitSearch(searchText)}
+        />
+        <Pressable style={{alignSelf: 'center', position: 'absolute', marginRight: 4, padding: 4}}
+        onPress={() => submitSearch("")}>
+          <MaterialIcons name="clear" size={16}/>
+        </Pressable>
+      </ThemedView>
       <ImageButton
         onPress={() => submitSearch(searchText)}
         source={require("@/assets/images/search-icon.png")}
@@ -35,13 +42,18 @@ export default function SearchBar({query} : {query: string}) {
 }
 
 const styles = StyleSheet.create({
-  searchBox: {
+  searchContainer: {
+    flexDirection: 'row-reverse',
     flex: 1,
     borderWidth: 0.5,
     borderRadius: 16,
+    backgroundColor: "white",
+  },
+  searchBox: {
+    flex: 1,
     paddingLeft: 8,
     paddingRight: 8,
-    backgroundColor: "white",
+    borderRadius: 16,
   },
   titleContainer: {
     flexDirection: "row",
